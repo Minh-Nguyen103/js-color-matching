cb665c68956f7a360c3cc8cd07f54408ad615970
Minh-Nguyen103 <pmnguyen1032000@gmail.com>
2021-11-02 16:53:41 +0700

    random color Pairs

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,40 @@
+function shuffle(arr) {
+  if (!Array.isArray(arr) || arr.length < 1) return
+
+  for (let index = arr.length - 1; index > 1; index--) {
+    const j = Math.floor(Math.random() * index)
+
+    const temp = arr[index]
+    arr[index] = arr[j]
+    arr[j] = temp
+  }
+
+  return arr
+}
+
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+
+  if (typeof count !== 'number' && count < 0) return []
+
+  const colorList = []
+
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome']
+
+  for (let i = 0; i < count; i++) {
+    //randomColor function provided by lib: https://github.com/davidmerfield/randomColor
+    const color = window.randomColor({
+      luminosity: 'dark',
+      hue: hueList[i % hueList.length],
+    })
+    colorList.push(color)
+  }
+
+  const fullColor = [...colorList, ...colorList]
+
+  //shuffle
+  shuffle(fullColor)
+
+  return fullColor
 }
